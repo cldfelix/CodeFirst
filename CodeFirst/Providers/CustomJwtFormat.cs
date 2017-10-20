207c8cde5654943911ddea5826f6bab050b9d809
Claudinei Felix <claudinei.felix@outlook.com>
2017-10-20 11:30:56 -0200

    adicionado ferramento de token

---
+++ CodeFirst/Providers/CustomJwtFormat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+
+namespace CodeFirst.Providers
+{
+    public class CustomJwtFormat : ISecureDataFormat<AuthenticationTicket>
+    {
+        private readonly string _issuer;
+
+        public CustomJwtFormat(string issuer)
+        {
+            _issuer = issuer;
+        }
+
+        public string Protect(AuthenticationTicket data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException("data");
+            }
+
+            var audienceId = ConfigurationManager.AppSettings["as:AudienceId"];
+            var symmetricKeyAsBase64 = ConfigurationManager.AppSettings["as:AudienceSecret"];
+            var keyByteArray = TextEncodings.Base64Url.Decode(symmetricKeyAsBase64);
+            //var signingKey = new HmacSigningCredentials(keyByteArray);
+            var securityKey = new SymmetricSecurityKey(keyByteArray);
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+            var issued = data.Properties.IssuedUtc;
+            var expires = data.Properties.ExpiresUtc;
+            var token = new JwtSecurityToken(_issuer, audienceId, data.Identity.Claims, issued.Value.UtcDateTime, expires.Value.UtcDateTime, signingCredentials);
+            var handler = new JwtSecurityTokenHandler();
+
+            var jwt = handler.WriteToken(token);
+
+            return jwt;
+        }
+
+        public AuthenticationTicket Unprotect(string protectedText)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
